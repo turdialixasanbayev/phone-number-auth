8d5e7d83aee6a9823218337293721b0e3ad3f22a
Turdiali <turdialixasanbayev@gmail.com>
2025-08-15 19:47:49 +0500

    `Added current year to global context and footer in home template`

--- core/global_context.py
+++ core/global_context.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
+
 def global_context(request):
     context = {
         'site_name': 'Phone Number Auth',
-        'user': request.user
+        'user': request.user,
+        'current_year': datetime.now().year
     }
     return context
