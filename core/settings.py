dfd670a3a5184203bbd9746d723d34583bf5ac2a
Turdiali <turdialixasanbayev@gmail.com>
2025-08-15 21:11:31 +0500

    `Update settings.py: change DEBUG setting from True to False`

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i%=zi%*b-aw3@nb$lb8d*92u4w0m+m1t=pmv!m3aabgpkaagd3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
